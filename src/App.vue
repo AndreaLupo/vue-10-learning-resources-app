271561fb34930f80403f03b6c7349118c4f9f24d
Wolf Developer <lupo.andrea.93@gmail.com>
2022-11-19 11:24:19 +0100

    Basic learning component

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <ul>
+    <li>
+        <learning-resouce v-for="res in storedResources" :key="res.id"
+            :title="res.title" :description="res.description" :link="res.link">
+        </learning-resouce>
+    </li>
+  </ul>
+</template>
+
+<script>
+import LearningResouce from './components/learningResources/LearningResource.vue';
+
+export default {
+    components: {
+        LearningResouce
+    },
+    data() {
+        return {
+            storedResources: [
+                { 
+                    id: 'official-guide',
+                    title: 'Official Guide',
+                    description: 'The official Vue.js documentation',
+                    link: 'https://vuejs.org'
+                },
+                { 
+                    id: 'google',
+                    title: 'Google',
+                    description: 'Well, it\'s Google!',
+                    link: 'https://google.com'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
